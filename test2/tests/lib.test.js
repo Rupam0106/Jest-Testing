889b9c1dd4653107d6fae0cc0c1dbd5aee7d8fd8
rupam0106 <rupam.gupta@rudrainnovative.in>
2023-06-19 18:50:30 +0530

    test-2

--- test2/tests/lib.test.js
+++ test2/tests/lib.test.js
@@ -2,9 +2,11 @@ const lib = require("../lib/lib");
 
 describe("registerUser", () => {
   it("should throw error if username is falshy", () => {
-    const args=[null,undefined,NaN,'',false];
-    args.forEach(a=>{
-      expect(()=>{
-        lib.registerUser(a)}).tothrow()})
-    })
+    const args = [null, undefined, NaN, "", false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
   });
+});
